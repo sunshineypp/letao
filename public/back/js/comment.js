c0cd1a57aaabf3f196ee8ce230d45b803251b954
yuan <yuan@qq.com>
2017-10-30 17:20:41 +0800

    分类显示与隐藏结束

--- public/back/js/comment.js
+++ public/back/js/comment.js
@@ -1,4 +1,5 @@
 $(function () {
+    //1.进度条
     $(document).ajaxStart(function () {
         //开启进度条
         NProgress.start();
@@ -7,4 +8,9 @@ $(function () {
         //关闭进度条
         NProgress.done();
     });
+
+    //分类显示与隐藏
+        $('.child').prev().on('click',function () {
+        $(this).next().slideToggle();
+    })
 });
