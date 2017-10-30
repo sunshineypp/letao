1482e754c72ee59c53e58d49e3ab4785dbac36ba
yuan <yuan@qq.com>
2017-10-30 17:56:58 +0800

    校验用户是否登录及给分页补充模态窗

--- public/back/js/comment.js
+++ public/back/js/comment.js
@@ -1,4 +1,18 @@
 $(function () {
+    //校验用户是否登录
+    if (location.href.indexOf('login.html') < 0) {
+        $.ajax({
+            type: 'get',
+            url: '/employee/checkRootLogin',
+            success: function (data) {
+                if (data.error === 400) {
+                    location.href = "login.html";
+                }
+            }
+        });
+    }
+
+
     //1.进度条
     $(document).ajaxStart(function () {
         //开启进度条
@@ -10,27 +24,27 @@ $(function () {
     });
 
     //2.分类显示与隐藏
-        $('.child').prev().on('click',function () {
+    $('.child').prev().on('click', function () {
         $(this).next().slideToggle();
     });
-    
+
     //3.左侧移动
-    $('.icon_menu').on('click',function () {
+    $('.icon_menu').on('click', function () {
         $('.lt_aside').toggleClass('now');
         $('.lt_main').toggleClass('now');
     });
 
     //4.退出功能
-    $('.icon_logout').on('click',function () {
+    $('.icon_logout').on('click', function () {
         $('#logoutModal').modal('show');
     });
-    $('.btn_logout').on('click',function () {
+    $('.btn_logout').on('click', function () {
         //告诉后台要退出了
         $.ajax({
             type: 'get',
             url: '/employee/employeeLogout',
             success: function (data) {
-                if(data.success){
+                if (data.success) {
                     window.location.href = "login.html";
                 }
             }
