9cbe800402bb8e358aa9b7e96a1278269da0d147
yuan <yuan@qq.com>
2017-10-31 19:53:34 +0800

    补充一级分类添加分类功能

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -28,4 +28,56 @@ $(function () {
         });
     }
     render();
+
+    //添加分类
+    $('.btn_add').on('click',function () {
+        //让模态框显示
+        $('#addModal').modal('show');
+    });
+
+    var $form = $('#form');
+
+    $form.bootstrapValidator({
+        //校验时使用的图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields:{
+
+            //name属性
+            categoryName:{
+                validators:{
+                    notEmpty:{
+                        message:"一级分类名称不能为空"
+                    }
+                }
+            }
+
+        }
+    });
+    
+    //验证成功之后
+    $form.on('success.form.bv',function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: ' /category/addTopCategory',
+            data: $form.serialize(),
+            success: function (data) {
+                if(data.success){
+                    //让模态框显示
+                    $('#addModal').modal('hide');
+                    currentPage = 1;
+                    render();
+
+                    //重置表单
+                    $form.data('bootstrapValidator').resetForm();
+                    $form[0].reset();
+                }
+            }
+        });
+    });
 });
