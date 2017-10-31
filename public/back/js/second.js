30384d9b0caaa4f81ab7313a2d1743ab8e678bb8
yuan <yuan@qq.com>
2017-10-31 22:11:39 +0800

    二级分类完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -2,6 +2,8 @@ $(function () {
     var currentPage = 1;
     var pageSize = 5;
 
+    var $form = $('#form');
+
     function render() {
         $.ajax({
             type: 'get',
@@ -29,4 +31,103 @@ $(function () {
         });
     }
     render();
+
+    $('.btn_add').on('click',function () {
+        $('#addModal').modal('show');
+
+        //获取一级分类
+        $.ajax({
+            type: 'get',
+            url: '/category/queryTopCategoryPaging',
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            success: function (data) {
+                var html = template('tpl2',data);
+                $('.dropdown .dropdown-menu').html(html);
+            }
+        });
+    });
+
+
+    //点击下拉菜单按钮，设置值
+    $('.dropdown .dropdown-menu').on('click','a',function () {
+        $('.dropdown_text').html($(this).html());
+        $('#categoryId').val($(this).data('id'));
+
+        //categoryId通过校验
+        $form.data("bootstrapValidator").updateStatus("categoryId", "VALID");
+    });
+
+
+    //文件上传
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e,data) {
+            // console.log(data);
+            $(".img_box img").attr("src", data.result.picAddr);
+            $("#brandLogo").val( data.result.picAddr );
+            $form.data("bootstrapValidator").updateStatus("brandLogo", "VALID");
+        }
+    });
+
+    //校验
+    $form.bootstrapValidator({
+        //默认不校验的配置
+        excluded:[],
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            categoryId: {
+                validators: {
+                    notEmpty: {
+                        message: '请选择一级分类'
+                    }
+                }
+            },
+            brandName: {
+                validators: {
+                    notEmpty: {
+                        message: '请输入二级分类的名称'
+                    }
+                }
+            },
+            brandLogo: {
+                validators: {
+                    notEmpty: {
+                        message: '请上传图片'
+                    }
+                }
+            }
+        }
+    });
+
+    //校验成功后提交
+    $form.on('success.form.bv',function (e) {
+        e.preventDefault();
+        // console.log($form.serialize());
+        $.ajax({
+            type: 'post',
+            url:"/category/addSecondCategory",
+            data: $form.serialize(),
+            success: function (data) {
+                console.log(data);
+                if(data.success){
+                    //关闭模态框
+                    $('#addModal').modal('hide');
+                    //重新渲染第一页
+                    currentPage = 1;
+                    render();
+                    //重置表单
+                    $form[0].reset();
+                    $form.data('bootstrapValidator').resetForm();
+                }
+            }
+        });
+    });
+
 });
