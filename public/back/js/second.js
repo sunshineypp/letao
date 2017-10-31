f95539acc4d0a4bed5949f997ac6238c4fea1378
yuan <yuan@qq.com>
2017-10-31 18:49:33 +0800

    二级分类内容显示完成

---
+++ public/back/js/second.js
@@ -0,0 +1,32 @@
+$(function () {
+    var currentPage = 1;
+    var pageSize = 5;
+
+    function render() {
+        $.ajax({
+            type: 'get',
+            url: ' /category/querySecondCategoryPaging',
+            data: {
+                page: currentPage,
+                pageSize: pageSize
+            },
+            success: function (data) {
+                var html = template('tpl',data);
+                $('tbody').html(html);
+
+                //分页
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    currentPage: currentPage,
+                    size: 'small',
+                    totalPages: Math.ceil(data.total/pageSize),
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        currentPage = page;
+                        render();
+                    }
+                });
+            }
+        });
+    }
+    render();
+});
