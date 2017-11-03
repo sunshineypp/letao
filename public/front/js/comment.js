2f38dc7664817a71213b4f9899f428ff12c021fc
yuan <yuan@qq.com>
2017-11-03 21:31:38 +0800

    搜索详情页结束

---
+++ public/front/js/comment.js
@@ -0,0 +1,18 @@
+// console.log(location.search);//?txt=1
+var tools = {
+    getParamObj: function () {
+        var obj = {};
+        var search = location.search;
+        search = search.slice(1);
+        var arr = search.split('&');
+        for (var i = 0; i < arr.length; i++) {
+            var key = arr[i].split('=')[0];
+            var value = arr[i].split('=')[1];
+            obj[key] = value;
+        }
+        return obj;
+    },
+    getParam: function (key) {
+        return this.getParamObj()[key];
+    }
+};
--- public/front/js/searchList.js
+++ public/front/js/searchList.js
@@ -1,3 +1,79 @@
-/**
- * Created by Dell on 2017/11/3.
- */
+//滚动初始化
+var sc = mui('.mui-scroll-wrapper').scroll({
+    indicators: false
+});
+
+//获取地址栏的参数
+var key = tools.getParam("txt");
+// if(key == 'undefined'){
+//
+// }
+// console.log(key);
+//设置到输入框中
+$('.lt_search input').val(key);
+
+//搜索商品，渲染出来
+var data = {
+    proName: '',
+    brandId: '',
+    price: '',
+    num: '',
+    page: 1,
+    pageSize: 50
+};
+function render(data) {
+    $.ajax({
+        type: 'get',
+        url: '/product/queryProduct',
+        data: data,
+        success: function (data) {
+            console.log(data);
+            $('.lt_product').html(template('tpl', data));
+        }
+    });
+}
+data.proName = key;
+render(data);
+
+//搜索按钮功能
+$('.lt_search button').on('click', function () {
+    //清除所有a的now
+    $('.lt_sort a').removeClass('now');
+    $('.lt_sort span').removeClass('fa-angle-up').addClass('fa-angle-down');
+    //清除排序
+    data.price = '';
+    data.num = '';
+
+    //获取输入框的值，为空，提示
+    var txt = $('.lt_search input').val();
+    if (txt === "") {
+        mui.toast('请输入搜索内容');
+    }
+    else {
+        //渲染
+        data.proName = txt;
+        render(data);
+    }
+});
+
+//排序
+$('.lt_sort a[data-type]').on('click', function () {
+    $span = $(this).find('span');
+    if ($(this).hasClass('now')) {
+        $span.toggleClass('fa-angle-down').toggleClass('fa-angle-up');
+    } else {
+        //没有 移除兄弟的now 自己添加
+        $(this).addClass('now').siblings().removeClass('now');
+        $('.lt_sort span').removeClass('fa-angle-up').addClass('fa-angle-down');
+    }
+
+    //判断哪个排序
+    var type = $(this).data('type');
+    var value = $span.hasClass('fa-angle-down') ? 2 : 1;
+    //设置给data，渲染，先清空之前的数据
+    data.price = '';
+    data.num = '';
+    
+    data[type] = value;
+    render(data);
+});
